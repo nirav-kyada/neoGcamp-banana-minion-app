c177a8920627beb1f78fee6c47ec7735274e9ca4
Nirav Kyada <34131271+nirav-kyada@users.noreply.github.com>
2021-02-04 22:45:06 +0530

    changeed api to unlimited translation

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var btntranslate = document.querySelector('#btn-translate');
 var txtinput = document.querySelector('#txt-input');
 var outputDiv = document.querySelector('#txt-output');
 // console.log(txtinput);
-var serverURL2 =
-	'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
-var serverURL1 = 'https://api.funtranslations.com/translate/minion.json';
+var serverURL2 = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
+// var serverURL1 = 'https://api.funtranslations.com/translate/minion.json';
+var serverURL1 = 'https://minionsapi.nirav1.repl.co/translate/minion.json'; // unlimited api translation \
+//ref. - https://dev.to/rakesh/how-did-i-remove-the-rate-limit-from-my-minion-translator-and-how-you-can-do-it-too-a-neogcamp-story-2kgp
+
 function geneateURL(text) {
 	return serverURL1 + '?text=' + text;
 }
